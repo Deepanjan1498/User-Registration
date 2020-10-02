4ee2d8670a3d30186552b47dc9d2e534517ef6d3
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-03 00:26:57 +0530

    [Deepanjan]add. uc2 is solved.

--- User.java
+++ User.java
@@ -5,17 +5,17 @@ import java.util.regex.Pattern;
 public class User {
 
 	public static void main(String[] args) {
-		List<String> firstName = new ArrayList<String>();
+		List<String> lastName = new ArrayList<String>();
 
-		firstName.add("Deepanjan");
-		firstName.add("De");
-		firstName.add("e");
+		lastName.add("Sharma");
+		lastName.add("sharMa");
+		lastName.add("2sm");
 		String regex = "^[A-Z]{1}+[a-zA-Z]{3,}$";
 
 		Pattern pattern = Pattern.compile(regex);
 
-		for (String first : firstName) {
-			Matcher matcher = pattern.matcher(first);
+		for (String last : lastName) {
+			Matcher matcher = pattern.matcher(last);
 			System.out.println(matcher.matches());
 		}
 	}
