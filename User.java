b3429705418702fab74ee6241c4f85c4b1349a5e
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-03 00:13:12 +0530

    [Deepanjan]add. uc1 is solved.

---
+++ User.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class User {
+
+	public static void main(String[] args) {
+		List<String> firstName = new ArrayList<String>();
+
+		firstName.add("Deepanjan");
+		firstName.add("De");
+		firstName.add("e");
+		String regex = "^[A-Z]{1}+[a-zA-Z]{3,}$";
+
+		Pattern pattern = Pattern.compile(regex);
+
+		for (String first : firstName) {
+			Matcher matcher = pattern.matcher(first);
+			System.out.println(matcher.matches());
+		}
+	}
+}
